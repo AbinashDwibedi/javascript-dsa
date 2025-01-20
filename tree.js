2e1d180e7bac112d58d3f7f7c9f69c50a7868ec8
= <=>
2025-01-20 22:59:39 +0530

    basic tree and binary tree is added

--- tree.js
+++ tree.js
@@ -1,22 +1,64 @@
-// // let chunkedArray = [[]];
+// // Basic Tree any parent can contain's any number of nodes ;
 
-// // let arr = [1,2,3,4,5];
-// // let size = 2;
-// // let numOfChunks = Math.ceil(arr.length/size);
-// // console.log(numOfChunks)
-
-// var createCounter = function(init) {
-//     return {
-//         init,
-//         actualValue:init,
-//         increment : function(){
-//             ++this.init;
-//             return this.init
-//         }
+// class TreeNode{
+//     constructor(data){
+//         this.data = data;
+//         this.child = [];
 //     }
-// };
-// console.log(createCounter(1).increment())
-// // console.log(chunk([1,2,3,4,5],2))
+//     addNode(child){
+//         this.child.push(child);
+//         console.log(":: New data added successfully ::");
+//     }
+//     display(level = 0){
+//         console.log("".repeat(level*4),this.data);
+//         this.child.forEach((child)=>{
+//             child.display(++level);
+//         })
+//     }
+// }
+
+// const root = new TreeNode("root");
+// const child1 = new TreeNode("child 1");
+// const child2 = new TreeNode("child 2");
+// const grandChild1 = new TreeNode("grand child 1");
+// const grandChild2 = new TreeNode("grand child 2");
+// child1.addNode(grandChild1);
+// child2.addNode(grandChild2);
+// root.addNode(child1);
+// root.addNode(child2);
+// root.display();
 
 
-//not implemented
+// Representation of binary tree
+
+class TreeNode{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    addLeft(child){
+        this.left = child;
+    }
+    addRight(child){
+        this.right = child;
+    }
+}
+
+const root = new TreeNode("root");
+const left = new TreeNode("left");
+const right = new TreeNode("right");
+
+const leftLeft = new TreeNode("left left");
+const leftRight = new TreeNode("left right");
+
+left.addLeft(leftLeft);
+left.addRight(leftRight);
+
+root.addLeft(left);
+root.addRight(right);
+
+console.log(root);
+
+
+
